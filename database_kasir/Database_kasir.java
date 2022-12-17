14a9720cd25020deb2715cf701fd00ae61fe60fb
Adinda aulia sari <91654530+Adindaauliasari@users.noreply.github.com>
2022-12-17 14:20:35 +0700

    Add files via upload

--- database_kasir/Database_kasir.java
+++ database_kasir/Database_kasir.java
@@ -1,3 +1,7 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
 package database_kasir;
 
 import java.time.LocalDate;
@@ -17,10 +21,10 @@ public class Database_kasir {
        kategori ktg = new kategori(1, "barang hidup");
        ArrayList<kategori> uwaw = new ArrayList<>();
        uwaw.add(ktg);
-       barang brg = new barang("A0001", LocalDate.of(2021, Month.DECEMBER, 12), uwaw , "barang A", 10000, 3, 2500);
-       ktg.getProduk(brg);
+       barang brg = new barang("A0001", LocalDate.of(2020, Month.DECEMBER, 12), uwaw , "barang A", 10000, 3, 2500);
        
-       brg.getKategori();
+       ktg.getProduk(brg);
+       brg.isExpired();
     }
     
 }

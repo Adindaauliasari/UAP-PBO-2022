905515938bd4a7879decb10fe0cfc99b4c677bc9
Abdilah Asyidiqi <70487624+asid30@users.noreply.github.com>
2022-12-17 15:14:44 +0700

    Update Database_kasir.java
    bikin banyak instance

--- database_kasir/Database_kasir.java
+++ database_kasir/Database_kasir.java
@@ -1,30 +1,24 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package database_kasir;
 
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.ArrayList;
 
-/**
- *
- * @author ASUS
- */
 public class Database_kasir {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-       kategori ktg = new kategori(1, "barang hidup");
-       ArrayList<kategori> uwaw = new ArrayList<>();
-       uwaw.add(ktg);
-       barang brg = new barang("A0001", LocalDate.of(2020, Month.DECEMBER, 12), uwaw , "barang A", 10000, 3, 2500);
+        kategori ktg = new kategori(1, "barang hidup");
+        kategori ktg2 = new kategori(2, "barang ajaib");
        
-       ktg.getProduk(brg);
-       brg.isExpired();
+        ArrayList<kategori> uwaw = new ArrayList<>();
+       
+        uwaw.add(ktg);
+        uwaw.add(ktg2);
+
+        barang brg = new barang("A0001", LocalDate.of(2024, Month.DECEMBER, 12), uwaw , "barang A", 10000, 3, 2500);
+        barang brg2 = new barang("A0002", LocalDate.of(2021, Month.DECEMBER, 12), uwaw , "barang B", 10000, 3, 2500);
+       
+        ktg.getProduk(brg,ktg);
+        System.out.println();
+        ktg2.getProduk(brg2,ktg2);
     }
-    
 }

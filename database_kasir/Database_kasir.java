c22041ae324fc4a9d8410e74f0d52388f5b2c9d0
Abdilah Asyidiqi <70487624+asid30@users.noreply.github.com>
2022-12-17 15:19:27 +0700

    bikin lebih dari 1 objek

--- database_kasir/Database_kasir.java
+++ database_kasir/Database_kasir.java
@@ -6,16 +6,16 @@ import java.util.ArrayList;
 
 public class Database_kasir {
     public static void main(String[] args) {
-        kategori ktg = new kategori(1, "barang hidup");
-        kategori ktg2 = new kategori(2, "barang ajaib");
+        kategori ktg = new kategori(1, "barang hidup");//object 1
+        kategori ktg2 = new kategori(2, "barang ajaib");//object 2
        
         ArrayList<kategori> uwaw = new ArrayList<>();
        
-        uwaw.add(ktg);
-        uwaw.add(ktg2);
+        uwaw.add(ktg);//object 1
+        uwaw.add(ktg2);//object 2
 
-        barang brg = new barang("A0001", LocalDate.of(2024, Month.DECEMBER, 12), uwaw , "barang A", 10000, 3, 2500);
-        barang brg2 = new barang("A0002", LocalDate.of(2021, Month.DECEMBER, 12), uwaw , "barang B", 10000, 3, 2500);
+        barang brg = new barang("A0001", LocalDate.of(2024, Month.DECEMBER, 12), uwaw , "barang A", 10000, 3, 2500);//object 1
+        barang brg2 = new barang("A0002", LocalDate.of(2021, Month.DECEMBER, 12), uwaw , "barang B", 10000, 3, 2500);//object 2
        
         ktg.getProduk(brg,ktg);
         System.out.println();

21e3ad535fbfac4ce0be87783745457d6ccb9964
Adinda aulia sari <91654530+Adindaauliasari@users.noreply.github.com>
2022-12-17 14:37:50 +0700

    Add files via upload

--- database_kasir/kategoriModel.java
+++ database_kasir/kategoriModel.java
@@ -1,9 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package database_kasir;
+
 import db.DBHelper;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+/**
+ *
+ * @author A C E R
+ */
 public class kategoriModel {
     private final Connection koneksi;
 

950c1b09e254c07063173f201206a83690918f83
Abdilah Asyidiqi <70487624+asid30@users.noreply.github.com>
2022-12-17 21:36:44 +0700

    Versi kedua
    isinya cuma ada tabel makan, barang, kategori_barang

--- sql_kasir/database_kasir.sql
+++ sql_kasir/database_kasir.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 13, 2022 at 02:30 PM
+-- Generation Time: Dec 17, 2022 at 02:38 PM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 8.1.10
 
@@ -28,18 +28,40 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `barang` (
-  `barcode` int(11) NOT NULL,
-  `expired` varchar(255) NOT NULL,
-  `id_kategori` int(11) NOT NULL
+  `barcode` varchar(5) NOT NULL,
+  `expired` date NOT NULL,
+  `kategori_barang` int(11) NOT NULL,
+  `nama_barang` varchar(255) NOT NULL,
+  `harga` double NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `diskon` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `kategori`
+-- Table structure for table `kategori_barang`
 --
 
-CREATE TABLE `kategori` (
+CREATE TABLE `kategori_barang` (
+  `id_kategori` int(11) NOT NULL,
+  `nama_kategori` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `kategori_barang`
+--
+
+INSERT INTO `kategori_barang` (`id_kategori`, `nama_kategori`) VALUES
+(1, 'barang mati');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kategori_makanan`
+--
+
+CREATE TABLE `kategori_makanan` (
   `id_kategori` int(11) NOT NULL,
   `nama_kategori` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -52,39 +74,12 @@ CREATE TABLE `kategori` (
 
 CREATE TABLE `makanan` (
   `id` int(11) NOT NULL,
-  `daya_tahan` int(11) NOT NULL,
-  `id_kategori` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `penjualan`
---
-
-CREATE TABLE `penjualan` (
-  `id_penjualan` int(11) NOT NULL,
-  `jumlah_produk` int(11) NOT NULL,
-  `stok` int(11) NOT NULL,
-  `pajak` double NOT NULL,
-  `total_bayar` double NOT NULL,
-  `id_produk` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `produk`
---
-
-CREATE TABLE `produk` (
-  `id_produk` int(11) NOT NULL,
-  `nama_produk` varchar(255) NOT NULL,
+  `daya_tahan` date NOT NULL,
+  `kategori_makanan` int(11) NOT NULL,
+  `nama_makanan` varchar(255) NOT NULL,
   `harga` double NOT NULL,
   `jumlah` int(11) NOT NULL,
-  `diskon` double NOT NULL,
-  `barcode_barang` int(11) NOT NULL,
-  `id_makanan` int(11) NOT NULL
+  `diskon` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -96,12 +91,18 @@ CREATE TABLE `produk` (
 --
 ALTER TABLE `barang`
   ADD PRIMARY KEY (`barcode`),
-  ADD KEY `id_kategori` (`id_kategori`);
+  ADD KEY `kategori_barang` (`kategori_barang`);
 
 --
--- Indexes for table `kategori`
+-- Indexes for table `kategori_barang`
 --
-ALTER TABLE `kategori`
+ALTER TABLE `kategori_barang`
+  ADD PRIMARY KEY (`id_kategori`);
+
+--
+-- Indexes for table `kategori_makanan`
+--
+ALTER TABLE `kategori_makanan`
   ADD PRIMARY KEY (`id_kategori`);
 
 --
@@ -109,56 +110,7 @@ ALTER TABLE `kategori`
 --
 ALTER TABLE `makanan`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `id_kategori` (`id_kategori`);
-
---
--- Indexes for table `penjualan`
---
-ALTER TABLE `penjualan`
-  ADD PRIMARY KEY (`id_penjualan`),
-  ADD KEY `id_produk` (`id_produk`);
-
---
--- Indexes for table `produk`
---
-ALTER TABLE `produk`
-  ADD PRIMARY KEY (`id_produk`),
-  ADD KEY `barcode_barang` (`barcode_barang`),
-  ADD KEY `id_makanan` (`id_makanan`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `barang`
---
-ALTER TABLE `barang`
-  MODIFY `barcode` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `kategori`
---
-ALTER TABLE `kategori`
-  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `makanan`
---
-ALTER TABLE `makanan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `penjualan`
---
-ALTER TABLE `penjualan`
-  MODIFY `id_penjualan` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `produk`
---
-ALTER TABLE `produk`
-  MODIFY `id_produk` int(11) NOT NULL AUTO_INCREMENT;
+  ADD KEY `makanan_ibfk_1` (`kategori_makanan`);
 
 --
 -- Constraints for dumped tables
@@ -168,26 +120,13 @@ ALTER TABLE `produk`
 -- Constraints for table `barang`
 --
 ALTER TABLE `barang`
-  ADD CONSTRAINT `barang_ibfk_1` FOREIGN KEY (`id_kategori`) REFERENCES `kategori` (`id_kategori`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `barang_ibfk_1` FOREIGN KEY (`kategori_barang`) REFERENCES `kategori_barang` (`id_kategori`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `makanan`
 --
 ALTER TABLE `makanan`
-  ADD CONSTRAINT `makanan_ibfk_1` FOREIGN KEY (`id_kategori`) REFERENCES `kategori` (`id_kategori`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `penjualan`
---
-ALTER TABLE `penjualan`
-  ADD CONSTRAINT `penjualan_ibfk_1` FOREIGN KEY (`id_produk`) REFERENCES `produk` (`id_produk`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `produk`
---
-ALTER TABLE `produk`
-  ADD CONSTRAINT `produk_ibfk_1` FOREIGN KEY (`barcode_barang`) REFERENCES `barang` (`barcode`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `produk_ibfk_2` FOREIGN KEY (`id_makanan`) REFERENCES `makanan` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `makanan_ibfk_1` FOREIGN KEY (`kategori_makanan`) REFERENCES `kategori_makanan` (`id_kategori`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

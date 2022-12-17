594abf02f12223a803fa1cf1fe3f8406f6f35b58
Abdilah Asyidiqi <70487624+asid30@users.noreply.github.com>
2022-12-17 21:43:22 +0700

    Nambah object
    add barang sama add kategori, sisanya nyusul

--- database_kasir/Database_kasir.java
+++ database_kasir/Database_kasir.java
@@ -5,20 +5,33 @@ import java.time.Month;
 import java.util.ArrayList;
 
 public class Database_kasir {
+
     public static void main(String[] args) {
-        kategori ktg = new kategori(1, "barang hidup");//object 1
-        kategori ktg2 = new kategori(2, "barang ajaib");//object 2
+        
+        kategoriModel hem = new kategoriModel();
+        barangModel ham = new barangModel();
+        
+        kategori ktg = new kategori(1, "barang hidup");
+        kategori ktg2 = new kategori(2, "barang ajaib");
        
         ArrayList<kategori> uwaw = new ArrayList<>();
        
-        uwaw.add(ktg);//object 1
-        uwaw.add(ktg2);//object 2
+        uwaw.add(ktg);
+        uwaw.add(ktg2);
 
-        barang brg = new barang("A0001", LocalDate.of(2024, Month.DECEMBER, 12), uwaw , "barang A", 10000, 3, 2500);//object 1
-        barang brg2 = new barang("A0002", LocalDate.of(2021, Month.DECEMBER, 12), uwaw , "barang B", 10000, 3, 2500);//object 2
+        barang brg = new barang("A0001", LocalDate.of(2024, Month.DECEMBER, 12), uwaw , "barang A", 10000, 3, 2500);
+        barang brg2 = new barang("A0002", LocalDate.of(2021, Month.DECEMBER, 12), uwaw , "barang B", 10000, 3, 2500);
        
-        ktg.getProduk(brg,ktg);
-        System.out.println();
-        ktg2.getProduk(brg2,ktg2);
+//        ktg.getProduk(brg,ktg);
+//        System.out.println();
+//        ktg2.getProduk(brg2,ktg2);
+        
+//        System.out.println(ktg.getId_kategori());
+        
+        hem.addKategori(ktg);
+        hem.addKategori(ktg2);
+        
+        ham.addBarang(brg, ktg);
+        ham.addBarang(brg2, ktg2);
     }
 }

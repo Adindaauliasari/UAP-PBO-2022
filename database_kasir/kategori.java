345ff2dcd2a026c1ad6c8528bd10add1739113a1
Abdilah Asyidiqi <70487624+asid30@users.noreply.github.com>
2022-12-17 15:17:32 +0700

    ubah method getProduk
    ubah method --> public void getProduk(barang brg) public void getProduk(barang brg, kategori ktg)

--- database_kasir/kategori.java
+++ database_kasir/kategori.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package database_kasir;
 
-/**
- *
- * @author A C E R
- */
 public class kategori {
     private int id_kategori;
     private String nama_kategori;
@@ -17,26 +8,24 @@ public class kategori {
         this.id_kategori = id_kategori;
         this.nama_kategori = nama_kategori;
     }
-
     public int getId_kategori() {
         return id_kategori;
     }
-
     public void setId_kategori(int id_kategori) {
         this.id_kategori = id_kategori;
     }
-
     public String getNama_kategori() {
         return nama_kategori;
     }
-
     public void setNama_kategori(String nama_kategori) {
         this.nama_kategori = nama_kategori;
     }
-    
-    public void getProduk(barang brg) {
-        System.out.println(brg.getNama_produk());
-        System.out.println(brg.getHarga());
-        System.out.println(brg.getExpired());
+    public void getProduk(barang brg, kategori ktg){
+        System.out.println("Nama barang: "+brg.getNama_produk());
+        System.out.println("Harga barang: "+brg.getHarga());
+        System.out.println("Tanggal barang expired: "+brg.getExpired());
+        System.out.println("Kategori barang: "+ktg.getNama_kategori());
+        System.out.println("Id kategori barang: "+ktg.getId_kategori());
+        System.out.print("Status barang: ");brg.isExpired();
     }
 }

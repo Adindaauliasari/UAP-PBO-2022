e1f44936a3e9c5693769f8927f428b1eec97f2ed
Adinda aulia sari <91654530+Adindaauliasari@users.noreply.github.com>
2022-12-17 14:37:09 +0700

    Add files via upload

--- database_kasir/kategori.java
+++ database_kasir/kategori.java
@@ -1,3 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package database_kasir;
+
+/**
+ *
+ * @author A C E R
+ */
 public class kategori {
     private int id_kategori;
     private String nama_kategori;
@@ -23,7 +34,9 @@ public class kategori {
         this.nama_kategori = nama_kategori;
     }
     
-    public void getProduk() {
-    
+    public void getProduk(barang brg) {
+        System.out.println(brg.getNama_produk());
+        System.out.println(brg.getHarga());
+        System.out.println(brg.getExpired());
     }
 }

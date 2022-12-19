07d16af7669a7d057e1800e796a25ccfbb57c3da
Adinda aulia sari <91654530+Adindaauliasari@users.noreply.github.com>
2022-12-19 09:33:10 +0700

    Add files via upload

---
+++ database_kasir/MainController.java
@@ -0,0 +1,39 @@
+package database_kasir;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+
+public class MainController {
+
+    @FXML
+    private Button btnBarang;
+
+    @FXML
+    private Button btnMakanan;
+
+    @FXML
+    private Pane paneMenu;
+
+    @FXML
+    private Text txtHome1;
+
+    @FXML
+    void barangMenu(ActionEvent event) {
+
+    }
+
+    @FXML
+    void homeMenu(MouseEvent event) {
+
+    }
+
+    @FXML
+    void makananMenu(ActionEvent event) {
+
+    }
+
+}

d4ba8b5ba42284c16e6be3cab701fdee98da98dd
Abdilah Asyidiqi <70487624+asid30@users.noreply.github.com>
2022-12-19 11:22:43 +0700

    Update KategoriMakanan.java

--- database_kasir/makanan/KategoriMakanan.java
+++ database_kasir/makanan/KategoriMakanan.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package database_kasir;
 
-/**
- *
- * @author ASUS
- */
 public class KategoriMakanan {
     private int id_kategori;
     private String nama_kategori;

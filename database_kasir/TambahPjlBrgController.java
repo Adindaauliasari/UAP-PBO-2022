56d751a24cc2c72a8ada7e0f3acfc97af482be4a
Adinda aulia sari <91654530+Adindaauliasari@users.noreply.github.com>
2022-12-19 09:36:05 +0700

    Add files via upload

---
+++ database_kasir/TambahPjlBrgController.java
@@ -0,0 +1,69 @@
+package database_kasir;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+
+public class TambahPjlBrgController {
+
+    @FXML
+    private Button btnBarang;
+
+    @FXML
+    private Button btnMakanan;
+
+    @FXML
+    private Button btnTambahPjl;
+
+    @FXML
+    private TextField fieldIDPjl;
+
+    @FXML
+    private TextField fieldJmlBrg;
+
+    @FXML
+    private TextField fieldNamaBrg;
+
+    @FXML
+    private TextField fieldPajakBrg;
+
+    @FXML
+    private TextField fieldStokBrg;
+
+    @FXML
+    private Pane paneMenu;
+
+    @FXML
+    private Text txtBarang;
+
+    @FXML
+    private Text txtHome;
+
+    @FXML
+    private Text txtPenjualan;
+
+    @FXML
+    void barangMenu(MouseEvent event) {
+
+    }
+
+    @FXML
+    void homeMenu(MouseEvent event) {
+
+    }
+
+    @FXML
+    void makananMenu(ActionEvent event) {
+
+    }
+
+    @FXML
+    void penjualanMenu(ActionEvent event) {
+
+    }
+
+}

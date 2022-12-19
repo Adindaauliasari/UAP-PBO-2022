2bb0c70bfc02ab29c3b4b1c828c3d5811b1e96e1
Abdilah Asyidiqi <70487624+asid30@users.noreply.github.com>
2022-12-19 10:30:44 +0700

    Update kategori.java

--- database_kasir/barang/kategori.java
+++ database_kasir/barang/kategori.java
@@ -1,31 +1,39 @@
 package database_kasir;
 
-public class kategori {
+public class KategoriBarang {
     private int id_kategori;
     private String nama_kategori;
+    private KategoriBarang ktg;
 
-    public kategori(int id_kategori, String nama_kategori) {
+    public KategoriBarang(int id_kategori, String nama_kategori) {
         this.id_kategori = id_kategori;
         this.nama_kategori = nama_kategori;
     }
+
+    public KategoriBarang(int id_kategori) {
+        this.id_kategori = id_kategori;
+        this.nama_kategori = "";
+    }
+    
     public int getId_kategori() {
         return id_kategori;
     }
+
     public void setId_kategori(int id_kategori) {
         this.id_kategori = id_kategori;
     }
+
     public String getNama_kategori() {
         return nama_kategori;
     }
+
     public void setNama_kategori(String nama_kategori) {
         this.nama_kategori = nama_kategori;
     }
-    public void getProduk(barang brg, kategori ktg){
-        System.out.println("Nama barang: "+brg.getNama_produk());
-        System.out.println("Harga barang: "+brg.getHarga());
-        System.out.println("Tanggal barang expired: "+brg.getExpired());
-        System.out.println("Kategori barang: "+ktg.getNama_kategori());
-        System.out.println("Id kategori barang: "+ktg.getId_kategori());
-        System.out.print("Status barang: ");brg.isExpired();
+    
+    public void getProduk(Barang brg) {
+        System.out.println(brg.getNama_produk());
+        System.out.println(brg.getHarga());
+        System.out.println(brg.getExpired());
     }
 }

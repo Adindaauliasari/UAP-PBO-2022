ff509a4fcaef0511405f929c8d79f524365d0a90
Abdilah Asyidiqi <70487624+asid30@users.noreply.github.com>
2022-12-19 11:05:32 +0700

    Add files via upload

--- sql_kasir/database_kasir.sql
+++ sql_kasir/database_kasir.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 17, 2022 at 02:38 PM
--- Server version: 10.4.25-MariaDB
--- PHP Version: 8.1.10
+-- Waktu pembuatan: 19 Des 2022 pada 05.03
+-- Versi server: 10.4.25-MariaDB
+-- Versi PHP: 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `barang`
+-- Struktur dari tabel `barang`
 --
 
 CREATE TABLE `barang` (
@@ -37,10 +37,18 @@ CREATE TABLE `barang` (
   `diskon` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data untuk tabel `barang`
+--
+
+INSERT INTO `barang` (`barcode`, `expired`, `kategori_barang`, `nama_barang`, `harga`, `jumlah`, `diskon`) VALUES
+('A0002', '2022-12-17', 1, 'barang A', 10000, 3, 2500),
+('A0004', '2022-01-17', 2, 'barang AB', 15000, 7, 1000);
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `kategori_barang`
+-- Struktur dari tabel `kategori_barang`
 --
 
 CREATE TABLE `kategori_barang` (
@@ -49,16 +57,17 @@ CREATE TABLE `kategori_barang` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `kategori_barang`
+-- Dumping data untuk tabel `kategori_barang`
 --
 
 INSERT INTO `kategori_barang` (`id_kategori`, `nama_kategori`) VALUES
-(1, 'barang mati');
+(1, 'barang keren'),
+(2, 'barang ajaib');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `kategori_makanan`
+-- Struktur dari tabel `kategori_makanan`
 --
 
 CREATE TABLE `kategori_makanan` (
@@ -69,7 +78,7 @@ CREATE TABLE `kategori_makanan` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `makanan`
+-- Struktur dari tabel `makanan`
 --
 
 CREATE TABLE `makanan` (
@@ -82,54 +91,107 @@ CREATE TABLE `makanan` (
   `diskon` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `penjualan_barang`
+--
+
+CREATE TABLE `penjualan_barang` (
+  `id_penjualan` int(11) NOT NULL,
+  `listBarang` varchar(5) NOT NULL,
+  `jumlahBarang` int(11) NOT NULL,
+  `stok` int(11) NOT NULL,
+  `pajak` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `penjualan_makanan`
+--
+
+CREATE TABLE `penjualan_makanan` (
+  `id_penjualan` int(11) NOT NULL,
+  `listMakanan` int(11) NOT NULL,
+  `jumlahMakanan` int(11) NOT NULL,
+  `stok` int(11) NOT NULL,
+  `pajak` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `barang`
+-- Indeks untuk tabel `barang`
 --
 ALTER TABLE `barang`
   ADD PRIMARY KEY (`barcode`),
   ADD KEY `kategori_barang` (`kategori_barang`);
 
 --
--- Indexes for table `kategori_barang`
+-- Indeks untuk tabel `kategori_barang`
 --
 ALTER TABLE `kategori_barang`
   ADD PRIMARY KEY (`id_kategori`);
 
 --
--- Indexes for table `kategori_makanan`
+-- Indeks untuk tabel `kategori_makanan`
 --
 ALTER TABLE `kategori_makanan`
   ADD PRIMARY KEY (`id_kategori`);
 
 --
--- Indexes for table `makanan`
+-- Indeks untuk tabel `makanan`
 --
 ALTER TABLE `makanan`
   ADD PRIMARY KEY (`id`),
   ADD KEY `makanan_ibfk_1` (`kategori_makanan`);
 
 --
--- Constraints for dumped tables
+-- Indeks untuk tabel `penjualan_barang`
+--
+ALTER TABLE `penjualan_barang`
+  ADD PRIMARY KEY (`id_penjualan`),
+  ADD KEY `listBarang` (`listBarang`);
+
+--
+-- Indeks untuk tabel `penjualan_makanan`
+--
+ALTER TABLE `penjualan_makanan`
+  ADD PRIMARY KEY (`id_penjualan`),
+  ADD KEY `listMakanan` (`listMakanan`);
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
 --
 
 --
--- Constraints for table `barang`
+-- Ketidakleluasaan untuk tabel `barang`
 --
 ALTER TABLE `barang`
   ADD CONSTRAINT `barang_ibfk_1` FOREIGN KEY (`kategori_barang`) REFERENCES `kategori_barang` (`id_kategori`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `makanan`
+-- Ketidakleluasaan untuk tabel `makanan`
 --
 ALTER TABLE `makanan`
   ADD CONSTRAINT `makanan_ibfk_1` FOREIGN KEY (`kategori_makanan`) REFERENCES `kategori_makanan` (`id_kategori`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `penjualan_barang`
+--
+ALTER TABLE `penjualan_barang`
+  ADD CONSTRAINT `penjualan_barang_ibfk_1` FOREIGN KEY (`listBarang`) REFERENCES `barang` (`barcode`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `penjualan_makanan`
+--
+ALTER TABLE `penjualan_makanan`
+  ADD CONSTRAINT `penjualan_makanan_ibfk_1` FOREIGN KEY (`listMakanan`) REFERENCES `makanan` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-

26eaf716467202b99d180778856b731a44bb055a
Abdilah Asyidiqi <70487624+asid30@users.noreply.github.com>
2022-12-19 10:42:43 +0700

    update parameter

--- database_kasir/barang/barangModel.java
+++ database_kasir/barang/barangModel.java
@@ -1,56 +1,74 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package database_kasir;
 
 import db.DBHelper;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-/**
- *
- * @author ASUS
- */
-public class barangModel {
+
+public class BarangModel {
     private final Connection koneksi;
 
-    public barangModel() {
+    public BarangModel() {
         this.koneksi = DBHelper.getConnection();
     }
     
-    public void addBarang(barang brg, kategori ktg){
-        String insert = "INSERT INTO `barang`(`barcode`, `expired`, `kategori_barang`, `nama_barang`, `harga`, `jumlah`, `diskon`) VALUES ('"+brg.getBarcode()+"','"+brg.getExpired()+"','"+ktg.getId_kategori()+"','"+brg.getNama_produk()+"','"+brg.getHarga()+"','"+brg.getJumlah()+"','"+brg.getDiskon()+"')";
+    public void addBarang(Barang brg) {
+        String insert = "INSERT INTO barang(barcode, expired, kategori_barang, "
+                + "nama_barang, harga, jumlah, diskon) VALUES ('"+brg.getBarcode()+"',"
+                + "'"+brg.getExpired()+"','"+brg.getId_kategori()+"',"
+                + "'"+brg.getNama_produk()+"','"+brg.getHarga()+"',"
+                + "'"+brg.getJumlah()+"','"+brg.getDiskon()+"')";
         
         try {
-            koneksi.createStatement().execute(insert);
-            System.out.println("Berhasil memasukan data");
+            koneksi.createStatement().executeUpdate(insert);
         } catch (SQLException ex) {
-            Logger.getLogger(barangModel.class.getName()).log(Level.SEVERE, null, ex);
-            System.out.println("Gagal memasukan data");
+            Logger.getLogger(BarangModel.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
     
-    public void updBarang(barang brg){
-        String update = "UPDATE barang SET barcode = ''"+brg.getBarcode()+"'', "
-                + "expired = ''"+brg.getExpired()+"'', id_kategori = ''"+brg.getKategori()+"'' "
-                + "WHERE barcode = '"+brg.getBarcode()+"'";
+    public void updBarang(Barang brg) {
+        String update = "UPDATE barang SET barcode='"+brg.getBarcode()+"',"
+                + "expired='"+brg.getExpired()+"',kategori_barang='"+brg.getId_kategori()+"',"
+                + "nama_barang='"+brg.getNama_produk()+"',harga='"+brg.getHarga()+"',"
+                + "jumlah='"+brg.getJumlah()+"',diskon='"+brg.getDiskon()+"' "
+                + "WHERE barcode='"+brg.getBarcode()+"'";
         
         try {
             koneksi.createStatement().executeUpdate(update);
         } catch (SQLException ex) {
-            Logger.getLogger(barangModel.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(BarangModel.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
     
-    public void delBarang(barang brg){
-        String delete = "DELETE FROM barang WHERE barcode = '"+brg.getBarcode()+"'";
+    public void delBarang(Barang brg) {
+        String delete = "DELETE FROM barang WHERE barcode='"+brg.getBarcode()+"'";
         
         try {
             koneksi.createStatement().executeUpdate(delete);
         } catch (SQLException ex) {
-            Logger.getLogger(barangModel.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(BarangModel.class.getName()).log(Level.SEVERE, null, ex);
         }
+    }
+    
+    public ArrayList<Barang> getBarang (){
+        String select = "SELECT * FROM `barang`";
+        ArrayList<Barang> brg = new ArrayList<Barang>();
+        
+        ResultSet rs;
+        try {
+            rs = koneksi.createStatement().executeQuery(select);
+            while(rs.next()){
+                Barang temp = new Barang(rs.getString("barcode"),rs.getDate("expired").toLocalDate(),rs.getInt("kategori_barang"),rs.getString("nama_barang"),rs.getDouble("harga"),rs.getInt("jumlah"),rs.getDouble("diskon"));
+                brg.add(temp);
+            }
+            System.out.println("Berhasil menampilkan data");
+        } catch (SQLException ex) {
+            Logger.getLogger(KategoriBarangModel.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("Gagal menampilkan data");
+        }
+        return brg;
     }
 }

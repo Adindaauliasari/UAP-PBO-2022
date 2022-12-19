57569720b73df8df8de2c93bacbe73dbf7ddad90
Adinda aulia sari <91654530+Adindaauliasari@users.noreply.github.com>
2022-12-19 09:35:22 +0700

    Add files via upload

---
+++ database_kasir/TambahKtgBrgController.java
@@ -0,0 +1,60 @@
+package database_kasir;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+
+public class TambahKtgBrgController {
+
+    @FXML
+    private Button btnBarang;
+
+    @FXML
+    private Button btnMakanan;
+
+    @FXML
+    private Button btnTambahKtgBrg;
+
+    @FXML
+    private TextField fieldIDKtgBrg;
+
+    @FXML
+    private TextField fieldNamaKtgBrg;
+
+    @FXML
+    private Pane paneMenu;
+
+    @FXML
+    private Text txtBarang;
+
+    @FXML
+    private Text txtHome;
+
+    @FXML
+    private Text txtKategori;
+
+    @FXML
+    void barangMenu(MouseEvent event) {
+
+    }
+
+    @FXML
+    void homeMenu(MouseEvent event) {
+
+    }
+
+    @FXML
+    void kategoriMenu(ActionEvent event) {
+
+    }
+
+    @FXML
+    void makananMenu(ActionEvent event) {
+
+    }
+
+}

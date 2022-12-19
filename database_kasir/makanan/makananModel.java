d9fc95368022752416b3cbaf2cd04d907e8d2160
Abdilah Asyidiqi <70487624+asid30@users.noreply.github.com>
2022-12-19 11:20:37 +0700

    Update makananModel.java

--- database_kasir/makanan/makananModel.java
+++ database_kasir/makanan/makananModel.java
@@ -1,50 +1,73 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package database_kasir;
 
 import db.DBHelper;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- *
- * @author ASUS
- */
-public class makananModel {
+public class MakananModel {
     private final Connection koneksi;
 
-    public makananModel(Connection koneksi) {
+    public MakananModel() {
         this.koneksi = DBHelper.getConnection();
     }
     
-    public void addMakanan(makanan mkn){
-        String insert = "INSERT INTO makanan(id, daya_tahan, id_kategori) VALUES "
-                + "(''"+mkn.getId()+"'',''"+mkn.getDaya_tahan()+"'',''"+mkn.getKategori()+"'')";
+    public void addMakanan(Makanan mkn) {
+        String insert = "INSERT INTO makanan(id, daya_tahan, kategori_makanan, "
+                + "nama_makanan, harga, jumlah, diskon) VALUES ('"+mkn.getId()+"',"
+                + "'"+mkn.getDaya_tahan()+"','"+mkn.getId_kategori()+"',"
+                + "'"+mkn.getNama_produk()+"','"+mkn.getHarga()+"',"
+                + "'"+mkn.getJumlah()+"','"+mkn.getDiskon()+"')";
         
         try {
             koneksi.createStatement().executeUpdate(insert);
         } catch (SQLException ex) {
-            Logger.getLogger(makananModel.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(MakananModel.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
     
-    public void updMakanan(makanan mkn){
-        String update = "UPDATE makanan SET id = ''"+mkn.getId()+"'', daya_tahan "
-                + "= ''"+mkn.getDaya_tahan()+"'', id_kategori = ''"+mkn.getKategori()+"'' "
-                + "WHERE id = '"+mkn.getId()+"'";
+    public void updMakanan(Makanan mkn) {
+        String update = "UPDATE makanan SET id='"+mkn.getId()+"',daya_tahan='"+mkn.getDaya_tahan()+"',"
+                + "kategori_makanan='"+mkn.getId_kategori()+"',nama_makanan='"+mkn.getNama_produk()+"',"
+                + "harga='"+mkn.getHarga()+"',jumlah='"+mkn.getJumlah()+"',"
+                + "diskon='"+mkn.getDiskon()+"' WHERE id='"+mkn.getId()+"'";
         
         try {
             koneksi.createStatement().executeUpdate(update);
         } catch (SQLException ex) {
-            Logger.getLogger(makananModel.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(MakananModel.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
     
-    public void delMakanan(makanan mkn){
-        String delete = "";
+    public void delMakanan(Makanan mkn) {
+        String delete = "DELETE FROM makanan WHERE id='"+mkn.getId()+"'";
+        
+        try {
+            koneksi.createStatement().executeUpdate(delete);
+        } catch (SQLException ex) {
+            Logger.getLogger(MakananModel.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public ArrayList<Makanan> getMakanan (){
+        String select = "SELECT * FROM `makanan`";
+        ArrayList<Makanan> mkn = new ArrayList<Makanan>();
+        
+        ResultSet rs;
+        try {
+            rs = koneksi.createStatement().executeQuery(select);
+            while(rs.next()){
+                Makanan temp = new Makanan(rs.getInt("id"),rs.getDate("daya_tahan").toLocalDate(),rs.getInt("kategori_makanan"),rs.getString("nama_makanan"),rs.getDouble("harga"),rs.getInt("jumlah"),rs.getDouble("diskon"));
+                mkn.add(temp);
+            }
+            System.out.println("Berhasil menampilkan data");
+        } catch (SQLException ex) {
+            Logger.getLogger(KategoriBarangModel.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("Gagal menampilkan data");
+        }
+        return mkn;
     }
 }

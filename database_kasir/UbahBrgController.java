3d8590e993ca52e38474570b2aa353f226aeff7a
Adinda aulia sari <91654530+Adindaauliasari@users.noreply.github.com>
2022-12-19 09:37:21 +0700

    Add files via upload

---
+++ database_kasir/UbahBrgController.java
@@ -0,0 +1,79 @@
+package database_kasir;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+
+public class UbahBrgController {
+
+    @FXML
+    private Button btnBarang;
+
+    @FXML
+    private Button btnMakanan;
+
+    @FXML
+    private Button btnUbahBarang;
+
+    @FXML
+    private TextField fieldBarcodeSebelum;
+
+    @FXML
+    private TextField fieldBarcodeSesudah;
+
+    @FXML
+    private TextField fieldBulan;
+
+    @FXML
+    private TextField fieldDiskon;
+
+    @FXML
+    private TextField fieldHarga;
+
+    @FXML
+    private TextField fieldJumlah;
+
+    @FXML
+    private TextField fieldKategori;
+
+    @FXML
+    private TextField fieldNama;
+
+    @FXML
+    private TextField fieldTahun;
+
+    @FXML
+    private TextField fieldTanggal;
+
+    @FXML
+    private Pane paneMenu;
+
+    @FXML
+    private Text txtBarang;
+
+    @FXML
+    private Text txtHome;
+
+    @FXML
+    private Text txtUbah;
+
+    @FXML
+    void barangMenu(ActionEvent event) {
+
+    }
+
+    @FXML
+    void homeMenu(MouseEvent event) {
+
+    }
+
+    @FXML
+    void makananMenu(ActionEvent event) {
+
+    }
+
+}

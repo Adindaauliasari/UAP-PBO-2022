d8aa4f31735d4e9f4c1c4ccf5b6f4395b6eb4e82
Adinda aulia sari <91654530+Adindaauliasari@users.noreply.github.com>
2022-12-19 09:30:37 +0700

    Add files via upload

---
+++ database_kasir/HapusPjlBrgController.java
@@ -0,0 +1,57 @@
+package database_kasir;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+
+public class HapusPjlBrgController {
+
+    @FXML
+    private Button btnBarang;
+
+    @FXML
+    private Button btnHapusPjl;
+
+    @FXML
+    private Button btnMakanan;
+
+    @FXML
+    private TextField fieldIDPjl;
+
+    @FXML
+    private Pane paneMenu;
+
+    @FXML
+    private Text txtBarang;
+
+    @FXML
+    private Text txtHome;
+
+    @FXML
+    private Text txtPenjualan;
+
+    @FXML
+    void barangMenu(MouseEvent event) {
+
+    }
+
+    @FXML
+    void homeMenu(MouseEvent event) {
+
+    }
+
+    @FXML
+    void makananMenu(ActionEvent event) {
+
+    }
+
+    @FXML
+    void penjualanMenu(ActionEvent event) {
+
+    }
+
+}

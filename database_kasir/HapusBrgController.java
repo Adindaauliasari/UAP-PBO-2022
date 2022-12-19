ddbbdbff5ca4f3ef12ed077f882864f8b0480875
Adinda aulia sari <91654530+Adindaauliasari@users.noreply.github.com>
2022-12-19 09:29:28 +0700

    Add files via upload

---
+++ database_kasir/HapusBrgController.java
@@ -0,0 +1,49 @@
+package database_kasir;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+
+public class HapusBrgController {
+
+    @FXML
+    private Button btnBarang;
+
+    @FXML
+    private Button btnHapusBrg;
+
+    @FXML
+    private Button btnMakanan;
+
+    @FXML
+    private TextField fieldBarcode;
+
+    @FXML
+    private Pane paneMenu;
+
+    @FXML
+    private Text txtBarang;
+
+    @FXML
+    private Text txtHome;
+
+    @FXML
+    void barangMenu(ActionEvent event) {
+
+    }
+
+    @FXML
+    void homeMenu(MouseEvent event) {
+
+    }
+
+    @FXML
+    void makananMenu(ActionEvent event) {
+
+    }
+
+}

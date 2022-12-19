af8556158b6b6c88d3cbe11e5b1cf6fbcc087db7
Abdilah Asyidiqi <70487624+asid30@users.noreply.github.com>
2022-12-19 10:44:51 +0700

    Update kategoriModel.java

--- database_kasir/barang/kategoriModel.java
+++ database_kasir/barang/kategoriModel.java
@@ -1,56 +1,68 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package database_kasir;
 
 import db.DBHelper;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.sql.ResultSet;
 
-/**
- *
- * @author A C E R
- */
-public class kategoriModel {
+public class KategoriBarangModel {
     private final Connection koneksi;
 
-    public kategoriModel() {
+    public KategoriBarangModel() {
         this.koneksi = DBHelper.getConnection();
     }
     
-    public void addKategori(kategori ktg){
-        String insert ="INSERT INTO kategori (id_kategori, nama_kategori) values "
-                + "('"+ktg.getId_kategori()+"', '"+ktg.getNama_kategori()+"')";
+    public void addKategori(KategoriBarang ktg){
+        String insert ="INSERT INTO kategori_barang(id_kategori, nama_kategori) values "
+                + "('"+ktg.getId_kategori()+"','"+ktg.getNama_kategori()+"')";
         
         try {
             koneksi.createStatement().executeUpdate(insert);
         } catch (SQLException ex) {
-            Logger.getLogger(kategoriModel.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(KategoriBarangModel.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
     
-    public void updKategori(kategori ktg){
-        String update ="UPDATE kategori SET nama_kategori = '"+ktg.getNama_kategori()+"' "
-                + "WHERE id_kategori = '"+ktg.getId_kategori()+"'";
+    public void updKategori(KategoriBarang ktg){
+        String update ="UPDATE kategori_barang SET nama_kategori = '"+ktg.getNama_kategori()+"' "
+                + "WHERE id_kategori='"+ktg.getId_kategori()+"'";
         
         try {
             koneksi.createStatement().executeUpdate(update);
         } catch (SQLException ex) {
-            Logger.getLogger(kategoriModel.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(KategoriBarangModel.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
     
-    public void delKategori(kategori ktg){
-        String delete = "DELETE FROM kategori WHERE id_kategori = '"+ktg.getId_kategori()+"'";
+    public void delKategori(KategoriBarang ktg){
+        String delete = "DELETE FROM kategori_barang WHERE id_kategori='"+ktg.getId_kategori()+"'";
         
         try {
             koneksi.createStatement().executeUpdate(delete);
         } catch (SQLException ex) {
-            Logger.getLogger(kategoriModel.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(KategoriBarangModel.class.getName()).log(Level.SEVERE, null, ex);
         }
+    }
+    
+    public ArrayList<KategoriBarang> getKategori (){
+        String select = "SELECT * FROM `kategori_barang`";
+        ArrayList<KategoriBarang> ktg = new ArrayList<KategoriBarang>();
+        
+        ResultSet rs;
+        try {
+            rs = koneksi.createStatement().executeQuery(select);
+            while(rs.next()){
+                KategoriBarang temp = new KategoriBarang(rs.getInt("id_kategori"),rs.getString("nama_kategori"));
+                ktg.add(temp);
+            }
+            System.out.println("Berhasil menampilkan data");
+        } catch (SQLException ex) {
+            Logger.getLogger(KategoriBarangModel.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("Gagal menampilkan data");
+        }
+        return ktg;
     }
 }
